78ecb3cf7d7e33e9cbc683f1555b7cbc7701a030
Antonio Jurlina <antonio.jurlina@maine.edu>
2020-04-04 17:56:41 -0400

    2020-04-02 update 2

--- scripts/data import.R
+++ scripts/data import.R
@@ -6,7 +6,8 @@ directory <- paste0(here::here(), "/data")
 setwd(directory)
 
 covid_data <- read_csv("montenegro_covid19.csv") %>%
-  mutate(date = lubridate::mdy(date))
+  mutate(date = lubridate::mdy(date),
+         `pop/km_sq` = population / km_sq)
 
 # save the data frame for further analysis
 save(covid_data, file = "montenegro_covid19.RData")
